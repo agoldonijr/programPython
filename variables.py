1804e21f637f90d70453eb06849342c115928e92
Alcides Goldoni Junior <goldoni@ggaunicamp.com>
2016-08-04 16:33:03 -0300

    alteraçao do variables.py

--- variables.py
+++ variables.py
@@ -1,5 +1,12 @@
 #!/bin/python3
 
-integer = 1;
-frase = 'eh nois brodi';
+frase  		= 'eh nois brodi';
+integer 	= 1;
+real		= 1.1
+realMaior	= 1.123456789
+
+print('frase: ', frase);
+print('inteiro: ', integer);
+print('real: ', real);
+print('real maior: ', realMaior);
 
