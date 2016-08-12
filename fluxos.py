29a5bd7da0946e9e4e4c7087c53d7a247172f59a
Alcides Goldoni Junior <goldoni@ggaunicamp.com>
2016-08-12 13:23:01 -0300

    for e if funcionando em fluxos.py

---
+++ fluxos.py
@@ -0,0 +1,7 @@
+nome 	= 'alcides'
+i = 0;
+for i in range(0, len(nome)):
+	if(nome[i] ==  'a' or nome[i] ==  'e' or nome[i] ==  'i' or nome[i] ==  'o' or nome[i] == 'u'   ):
+		print (nome[i], 'Vogal');
+	else:
+		print (nome[i]);
