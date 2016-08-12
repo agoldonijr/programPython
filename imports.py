cd040bc2ad6a0ee87c4d8832bcdcd83546ecf668
Alcides Goldoni Junior <goldoni@ggaunicamp.com>
2016-08-12 17:10:23 -0300

    importes de numpy

---
+++ imports.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+vetor = np.array([0,1,2,3,4,5]);
+
+print('Brincando com vetor');
+for i in vetor:
+	print(vetor[i]);
+
+print('Brincando com matrizes');
+matriz = np.array([ [1,2,3],[4,5,6],[7,8,9] ]);
+
+for i in
