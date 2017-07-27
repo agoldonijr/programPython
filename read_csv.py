fb089253d11e0d06db5105c34bb9d745197e1281
Alcides Goldoni Junior <goldoni@ggaunicamp.com>
2017-07-27 16:18:13 -0300

    nem sei o que foi feito

--- read_csv.py
+++ read_csv.py
@@ -6,79 +6,46 @@ dado = []
 cabecalho = []
 option = []
 
-def read_all(name_file):
-	i = 0
-	f = open(name_file, "r")
-	for r in csv.reader(open('Tequila-completo.csv','rb')):
-	  	if(i <= 24):
-			aux = f.readline()
-			cabecalho.append(aux.split(":"))
-			cabecalho[i][1] = cabecalho[i][1].strip()
-			cabecalho[i][2] = cabecalho[i][2].replace("Sampling frequency", "").strip()
-			cabecalho[i][3] = cabecalho[i][3].replace("Number of points","").strip()
-			cabecalho[i][4] = cabecalho[i][4].replace("X start","").strip()
-			cabecalho[i][5] = cabecalho[i][5].replace("Unit","").strip()
-			cabecalho[i][6] = cabecalho[i][6].replace("Domain Unit","").strip()
-			cabecalho[i][7] = cabecalho[i][7].strip()
-		elif(i>24) and (i<172):
-			aux = f.readline()
-			option.append(aux.split(":"))
-			#option[i][1] = option[i][1].strip()
-			
-		else:
-			aux = f.readline()
-			dado.append(aux)
-		i+=1
-	
-	print(cabecalho)
-	#print(option)
-	#print(dado)
-			
-
 #lendo header do arquivo
 def read_reader(name_file):
 	f = open(name_file, "r")
-
+	count = 0
 	#lendo as 24 primeiras linhas e salvando no 
-	for i in range (0,24):
-		aux = f.readline()
-		cabecalho.append(aux.split(":"))
-		cabecalho[i][1] = cabecalho[i][1].strip()
-		cabecalho[i][2] = cabecalho[i][2].replace("Sampling frequency", "").strip()
-		cabecalho[i][3] = cabecalho[i][3].replace("Number of points","").strip()
-		cabecalho[i][4] = cabecalho[i][4].replace("X start","").strip()
-		cabecalho[i][5] = cabecalho[i][5].replace("Unit","").strip()
-		cabecalho[i][6] = cabecalho[i][6].replace("Domain Unit","").strip()
-		cabecalho[i][7] = cabecalho[i][7].strip()
+	for i in open(name_file, "r"):
+		if count < 24:
+			aux = f.readline()
+			cabecalho.append(aux.split(":"))
+			cabecalho[count][1] = cabecalho[count][1].strip()
+			cabecalho[count][2] = cabecalho[count][2].replace("Sampling frequency", "").strip()
+			cabecalho[count][3] = cabecalho[count][3].replace("Number of points","").strip()
+			cabecalho[count][4] = cabecalho[count][4].replace("X start","").strip()
+			cabecalho[count][5] = cabecalho[count][5].replace("Unit","").strip()
+			cabecalho[count][6] = cabecalho[count][6].replace("Domain Unit","").strip()
+			cabecalho[count][7] = cabecalho[count][7].strip()
+		elif(count>=24 and count < 172):
+			aux = f.readline()
+			option.append(aux.split(":"))
+			option[count-24][1] = option[count-24][1].strip()
+		elif(count == 172):
+			dado.append(f.readline)
+		else:
+			dado.append(f.readline())
+		count+=1
 
-#	for i in range(0,24):
-#		print(cabecalho[i])
-
-#lendo opticoes do arquivo
-def read_option(name_file):
-	f = open(name_file, "r")
-
-	for i in range(24,172):
-		aux = f.readline()
-		option.append(aux.split(":"))
-		#option[i][1] = option[i][1].strip()
-
-	for i in range(24,172):
-		print(i, option[i])
-
-#lendo dados do arquivo
-#def read_data(name_file):
+	#print(cabecalho)
+	for i in dado:
+		print(i)
 
 #------------------------------------------------------------------------------
 
 #main
-name_file = raw_input('Nome do arquivo: ').strip();
+
+name_file = input('Nome do arquivo: ').strip();
 #name_file = 'Tequila-completo.csv'
 
 #leitura de cabecalho = validada
-#read_reader(name_file)
+read_reader(name_file)
 
 #leitura de opcoes = erro 
 #read_option(name_file)
 
-read_all(name_file)
